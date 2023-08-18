a999210d7b0fcd6fcd4d31fb31f97320bb603b51
Samir Goes <samir.goes@gmail.com>
2023-08-17 23:19:57 -0300

    Adiciona service e arquivos docker

---
+++ src/rinha-de-backend-api/Services/IPessoasService.cs
@@ -0,0 +1,10 @@
+using System.Linq.Expressions;
+
+namespace rinha_de_backend_api;
+
+interface IPessoasService
+{
+    Task<Pessoa> GetAsync(Expression<Func<Pessoa, Pessoa>> filter);
+    Task<Pessoa> AddAsync(Pessoa pessoa);
+    Task<IEnumerable<Pessoa>> ListAsync(Expression<Func<Pessoa, Pessoa>> filter);
+}
