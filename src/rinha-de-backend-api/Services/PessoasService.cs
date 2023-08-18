12aa2eb823b503944e915373d3d38cdd88a8a7b0
Samir Goes <samir.goes@gmail.com>
2023-08-17 23:20:21 -0300

    adiciona dockerfile

--- src/rinha-de-backend-api/Services/PessoasService.cs
+++ src/rinha-de-backend-api/Services/PessoasService.cs
@@ -1,9 +1,24 @@
-using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
 using MongoDB.Driver;
 
 namespace rinha_de_backend_api.Controllers;
 
-public class PessoasService
-(
+public class PessoasService : IPessoasService
+{
     private readonly IMongoCollection<Pessoa> _pessoasCollection;
-)
+
+    public Task<Pessoa> AddAsync(Pessoa pessoa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Pessoa> GetAsync(Expression<Func<Pessoa, Pessoa>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Pessoa>> ListAsync(Expression<Func<Pessoa, Pessoa>> filter)
+    {
+        throw new NotImplementedException();
+    }
+}
